dc0e7b92791188902bbde628e24550592ea58059
DopamineNone <c20160419raytheon@163.com>
2024-07-18 23:03:16 +0800

    refractor

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,15 +1,11 @@
 import axios from 'axios'
 
-const instance = axios.create({
+const request = axios.create({
   baseURL: 'http://localhost:3000/api',
   timeout: 1000
 })
 
-function setAuthorizationHeader(token) {
-  instance.defaults.headers.common['Authorization'] = `Bearer ${token}`
-}
-
-instance.interceptors.request.use((config) => {
+request.interceptors.request.use((config) => {
   //   console.log(typeof config.headers.Authorization === 'undefined')
   if (typeof config.headers.Authorization === 'undefined') {
     // get jwt token from store
@@ -17,4 +13,4 @@ instance.interceptors.request.use((config) => {
   return config
 })
 
-export { instance as axios, setAuthorizationHeader }
+export default request
